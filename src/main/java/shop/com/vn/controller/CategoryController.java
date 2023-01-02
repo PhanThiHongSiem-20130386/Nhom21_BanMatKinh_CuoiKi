064f27aebbf23b1eff714fb64a5dcb0c33e9a2fa
TRUONGPHANNGOCTRUC <20130449@st.hcmuaf.edu.vn>
2023-01-02 16:57:48 +0700

    phân trang

--- src/main/java/shop/com/vn/controller/CategoryController.java
+++ src/main/java/shop/com/vn/controller/CategoryController.java
@@ -16,15 +16,15 @@ public class CategoryController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String cid = request.getParameter("cid");
+        Integer cid = parseToNumber(request.getParameter("cid"));
        // List<Product> list = ProductService.getListCById(cid);
-        Category ca = ProductService.getNameC(Integer.parseInt(cid));
+        Category ca = ProductService.getNameC(cid);
         String checked = "checked";
         request.setAttribute("checked4", checked);
 
         request.setAttribute("nameC", ca);
         //request.setAttribute("list", list);
-//        request.setAttribute("cid",cid);
+        request.setAttribute("cid",cid);
 //        request.getAttribute(cid);
         request.getRequestDispatcher("category.jsp").forward(request, response);
 
@@ -33,5 +33,26 @@ public class CategoryController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+    private Integer parseToNumber(String receivedParam)
+    {
+        System.out.println("received param:"+receivedParam);
+        if (receivedParam != null && !receivedParam.trim().isEmpty())
+        {
+            try
+            {
+                return Integer.parseInt(receivedParam.trim());
+            }
+            catch (NumberFormatException nfe)
+            {
+                System.out.println("received param is not a number");
+                return null;
+            }
+        }
+        else
+        {
+            System.out.println("received param is null or empty");
+            return null;
+        }
     }
 }
